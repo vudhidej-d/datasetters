e8cd516a106b4a0eb71301b00af2349bd8a9cd69
Ton Vudhidej <vudhidej@muze.co.th>
2019-04-19 02:02:02 +0700

    DataCreateForm can add data to firestore

--- src/DataCreateForm.js
+++ src/DataCreateForm.js
@@ -1,50 +1,97 @@
-import React from 'react'
-import { Form, Select, Input, Button } from 'antd'
+import React, { useState } from 'react'
+import { Form, Select, Input, Button, notification } from 'antd'
+import { categories, whWords } from './shared'
 
+const { Option } = Select
 const { Item: FormItem } = Form
 const { TextArea } = Input
 
 const DataCreateForm = (props) => {
-  const { form: { getFieldDecorator } } = props
+  const {
+    form: { getFieldDecorator },
+    db,
+  } = props
+
+  const [loading, setLoading] = useState(false)
 
   const handleSubmit = (e) => {
     const { form: { validateFields } } = props
     e.preventDefault()
-    validateFields((err, values) => {
+    validateFields(async (err, values) => {
       if (err) return
-      console.log(values)
+      const categoryId = categories.findIndex((category) => category === values.category)
+      const whId = whWords.findIndex((whWord) => whWord === values.wh)
+      try {
+        setLoading(true)
+        await db.collection("dataset").add({
+          ...values,
+          categoryId,
+          whId,
+        })
+        notification['success']({
+          message: 'Data added successfully.',
+        });
+      } catch (e) {
+        console.error(e)
+        notification['error']({
+          message: 'Something went wrong, please try again.',
+        });
+      } finally {
+        setLoading(false)
+      }
     })
   }
 
   return (
     <Form>
       <FormItem label="Article ID">
-        {getFieldDecorator('articleId')(<Input />)}
+        {getFieldDecorator('articleId', {
+          rules: [{ required: true }],
+        })(<Input />)}
       </FormItem>
       <FormItem label="Article">
-        {getFieldDecorator('article')(<Input />)}
+        {getFieldDecorator('article', {
+          rules: [{ required: true }],
+        })(<Input />)}
       </FormItem>
       <FormItem label="Category">
-        {getFieldDecorator('category')(<Select />)}
+        {getFieldDecorator('category', {
+          initialValue: 'Science',
+          rules: [{ required: true }],
+        })(<Select>
+          {categories.map((category, index) => <Option value={category} key={index}>{category}</Option>)}
+        </Select>)}
       </FormItem>
       <FormItem label="Paragraph">
-        {getFieldDecorator('paragraph')(<TextArea />)}
+        {getFieldDecorator('paragraph', {
+          rules: [{ required: true }],
+        })(<TextArea />)}
       </FormItem>
       <FormItem label="WH Word">
-        {getFieldDecorator('wh')(<Select />)}
+        {getFieldDecorator('wh', {
+          initialValue: 'Who',
+          rules: [{ required: true }],
+        })(<Select>
+          {whWords.map((whWord, index) => (<Option value={whWord} key={index}>{whWord}</Option>))}
+        </Select>)}
       </FormItem>
       <FormItem label="Question">
-        {getFieldDecorator('question')(<Input />)}
+        {getFieldDecorator('question', {
+          rules: [{ required: true }],
+        })(<Input />)}
       </FormItem>
       <FormItem label="Answer">
-        {getFieldDecorator('answer')(<Input />)}
+        {getFieldDecorator('answer', {
+          rules: [{ required: true }],
+        })(<Input />)}
       </FormItem>
       <FormItem style={{ textAlign: 'center' }}>
         <Button
           onClick={handleSubmit}
           style={{ width: 100, height: 40 }}
           type="primary"
-        >Submit</Button>
+          loading={loading}
+        >Add</Button>
       </FormItem>
     </Form>
   )
