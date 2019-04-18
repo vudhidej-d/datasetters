394e64fea18bc711f850416946e0100384a84322
Ton Vudhidej <vudhidej@muze.co.th>
2019-04-19 02:01:05 +0700

    create shared for share value

---
+++ src/shared.js
@@ -0,0 +1,20 @@
+export const categories = [
+  'Science',
+  'History',
+  'Religion',
+  'Thailand',
+  'Art',
+  'Music',
+  'International',
+  'Technology',
+  'Health',
+  'Food',
+]
+
+export const whWords = [
+  'Who',
+  'When',
+  'Where',
+  'What',
+  'How + Adj/Adv',
+]
