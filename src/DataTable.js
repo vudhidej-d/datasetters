b0537275c4f4bdb75a4389accdfb7673f1a427dd
Ton Vudhidej <vudhidej@muze.co.th>
2019-04-19 02:12:28 +0700

    create DataTable

---
+++ src/DataTable.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Table } from 'antd'
+import { categories, whWords } from './shared'
+
+const columns = [{
+  title: 'Article ID',
+  dataIndex: 'articleId',
+  key: 'articleId',
+}, {
+  title: 'Article',
+  dataIndex: 'article',
+  key: 'article',
+}, {
+  title: 'Category',
+  dataIndex: 'category',
+  key: 'category',
+  filters: categories.map((category) => ({ text: category, value: category })),
+  onFilter: (value, record) => record.category.indexOf(value) === 0,
+}, {
+  title: 'Paragraph',
+  dataIndex: 'paragraph',
+  key: 'paragraph',
+}, {
+  title: 'WH Word',
+  dataIndex: 'wh',
+  key: 'wh',
+  filters: whWords.map((whWord) => ({ text: whWord, value: whWord })),
+  onFilter: (value, record) => record.wh.indexOf(value) === 0,
+}, {
+  title: 'Question',
+  dataIndex: 'question',
+  key: 'question',
+}, {
+  title: 'Answer',
+  dataIndex: 'answer',
+  key: 'answer',
+}];
+
+const DataTable = ({ dataSource }) => {
+  return (
+    <Table dataSource={dataSource} columns={columns} pagination={{ pageSize: 8 }}/>
+  )
+}
+
+export default DataTable
