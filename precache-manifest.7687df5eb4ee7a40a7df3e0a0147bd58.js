bb8e9bd3283ac300b208c2f824aac1a06e345c0f
Ton Vudhidej <vudhidej@muze.co.th>
2019-04-28 22:19:54 +0700

    Updates

---
+++ precache-manifest.7687df5eb4ee7a40a7df3e0a0147bd58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39467a6b17e7f31a02fe",
+    "url": "/datasetters/static/js/runtime~main.ea4e75fe.js"
+  },
+  {
+    "revision": "43ae00cfa7e0295c9870",
+    "url": "/datasetters/static/js/main.6c4e4700.chunk.js"
+  },
+  {
+    "revision": "183a1b81a7c785a047d5",
+    "url": "/datasetters/static/js/2.06e53f16.chunk.js"
+  },
+  {
+    "revision": "43ae00cfa7e0295c9870",
+    "url": "/datasetters/static/css/main.f0ac0bd1.chunk.css"
+  },
+  {
+    "revision": "183a1b81a7c785a047d5",
+    "url": "/datasetters/static/css/2.3cfeb0cc.chunk.css"
+  },
+  {
+    "revision": "d35f539aeb0dd7321c7bc720d1ac1051",
+    "url": "/datasetters/index.html"
+  }
+];
